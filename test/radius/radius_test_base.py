c8cbb1339dcda0c02bc9263f7da45032a283bc19
hxu <hao.xu@forescout.com>
2025-12-22 10:56:56 -0600

    Revert "Dev/AA-812: radius configuration on cisco switch"

--- test/radius/radius_test_base.py
+++ test/radius/radius_test_base.py
@@ -1,15 +1,13 @@
-from typing import cast
-
 from framework.log.logger import log
 from lib.ca.ca_common_base import CounterActBase
 from lib.ca.em import EnterpriseManager
 from lib.passthrough.passthrough_base import PassthroughBase
 from lib.plugin.radius.radius import Radius
 from lib.switch.cisco_ios import CiscoIOS
-from lib.switch.radius_factory import RadiusFactory
+from typing import cast
 
+class RadiusTestBase():
 
-class RadiusTestBase:
     def __init__(self, ca, em, radius, switch, passthrough, version="1.0.0"):
         self.ca = cast(CounterActBase, ca)
         self.em = cast(EnterpriseManager, em)
@@ -17,12 +15,9 @@ class RadiusTestBase:
         self.dot1x = cast(Radius, radius)
         self.switch = cast(CiscoIOS, switch)
         self.passthrough = cast(PassthroughBase, passthrough)
-        self.rf = RadiusFactory()
-        # Tests can construct and use RadiusFactory directly where needed
 
     def do_setup(self):
         log.info("radius common setup")
-        self.rf.setup(self.switch, port=self.switch.port1, radius_server_ip=self.ca.ipaddress, radius_secret="aristo")
 
     def radius_special_setup(self):
         log.info("radius special setup")
@@ -30,4 +25,3 @@ class RadiusTestBase:
     def do_teardown(self):
         # Teardown code to clean up after tests
         log.info("radius common teardown")
-        self.rf.teardown(self.switch, port=self.switch.port1, radius_server_ip=self.ca.ipaddress)
