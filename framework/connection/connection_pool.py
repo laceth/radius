0183b7bf6782a927011e03cad3b1463cec2a3ba0
hxu666 <hao.xu@forescout.com>
2026-02-24 19:19:35 -0600

    AA-875: invoke connection if broken

--- framework/connection/connection_pool.py
+++ framework/connection/connection_pool.py
@@ -5,12 +5,23 @@ class ConnectionPool:
     def __init__(self):
         self._pools = {}
 
-    def get(self, key, creator):
+    def get(self, key, creator=None):
+        if key in self._pools:
+            connection = self._pools[key]
+            try:
+                if not connection.is_alive():
+                    log.debug(f"[POOL] Connection for key: {key} is down. Reconnecting...")
+                    connection.close()
+                    raise Exception("Connection is down")
+            except Exception:
+                del self._pools[key]
+                log.debug(f"[POOL] Removed broken connection for key: {key}")
+
         if key not in self._pools:
+            if creator is None:
+                raise ValueError(f"No connection for key: {key} and no creator provided")
             log.debug(f"[POOL] Creating connection for key: {key}")
             self._pools[key] = creator()
-        else:
-            log.debug(f"[POOL] Reusing connection for key: {key}")
         return self._pools[key]
 
     def close_all(self):
