9f8384aab9bbf9a2f58685b8b4e22ab7d4b6280f
hxu666 <hao.xu@forescout.com>
2026-02-25 12:08:13 -0600

    AA-875: invoke dropped conenction for ca connections

--- framework/connection/connection_pool.py
+++ framework/connection/connection_pool.py
@@ -6,17 +6,26 @@ class ConnectionPool:
         self._pools = {}
 
     def get(self, key, creator=None):
+        """Get a connection from the pool by key. If the connection is not alive, it will be removed and recreated."""
         if key in self._pools:
             connection = self._pools[key]
             try:
-                if not connection.is_alive():
-                    log.debug(f"[POOL] Connection for key: {key} is down. Reconnecting...")
-                    connection.close()
-                    raise Exception("Connection is down")
-            except Exception:
+                # TODO: compromised way to check if connection is alive, better way is to abstract connections
+                if hasattr(connection, "is_alive"):
+                    # Netmiko connection
+                    if not connection.is_alive():
+                        connection.disconnect()
+                        raise ConnectionError("Netmiko connection is down")
+                elif hasattr(connection, "get_transport"):
+                    # Paramiko connection
+                    if not connection.get_transport() or not connection.get_transport().is_active():
+                        connection.close()
+                        raise ConnectionError("Paramiko connection is down")
+                else:
+                    raise TypeError("Unsupported connection type")
+            except ConnectionError:
                 del self._pools[key]
-                log.debug(f"[POOL] Removed broken connection for key: {key}")
-
+                log.info(f"[POOL] Removed broken connection for key: {key}")
         if key not in self._pools:
             if creator is None:
                 raise ValueError(f"No connection for key: {key} and no creator provided")
